bb3273a198612126526e81062701f2e0a1ece2f7
Ömer Faruk Yıldırım <omeeryldrmm03@gmail.com>
2024-03-27 21:11:25 +0300

    deneme 03 ve deneme 04 ekledim

--- src/deneme123/mainClass.java
+++ src/deneme123/mainClass.java
@@ -5,7 +5,8 @@ public class mainClass {
 	public static void main(String[] args) {
 		System.out.println("deneme1");
 		System.out.println("deneme2");
-
+		System.out.println("deneme3");
+		System.out.println("deneme4");
 	}
 
 }
